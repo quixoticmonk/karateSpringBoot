741add831614c51289d82dad609deb4627e5ac7f
devopsuser25 <manu.chandrasekhar@accenture.com>
2020-01-06 11:53:24 -0500

    + Gatling dependency +UserSimulation test calling karate feature

---
+++ src/test/java/karate/UserSimulation.scala
@@ -0,0 +1,14 @@
+package karate
+
+import com.intuit.karate.gatling.PreDef._
+import io.gatling.core.Predef._
+import scala.concurrent.duration._
+
+class UserSimulation extends Simulation{
+
+  val getAllBooks = scenario("Get all books").exec(karateFeature("classpath:karate/BookTest.feature"))
+
+  setUp(
+    getAllBooks.inject(rampUsers(10).during(10 seconds))
+  )
+}
